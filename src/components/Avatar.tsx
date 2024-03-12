3e5a412d0367b4686acba68a4afe9953d2b5a2ea
brighteast99 <brighteast99@gmail.com>
2024-03-12 17:50:19 +0900

    feat: 컴포넌트 정의 (Avatar)

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,44 @@
+import { FC, HTMLAttributes } from 'react'
+import { sizeLiteral } from '../types/commonProps'
+import { cva } from 'class-variance-authority'
+import { cn } from '../utils/handleClassName'
+
+export interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
+  /**
+   * Size of the avatar
+   */
+  size?: sizeLiteral
+  /**
+   * Source of the avatar image
+   */
+  imgSrc?: string
+}
+
+export const AvatarVariants = cva(`aspect-square overflow-clip rounded-full`, {
+  variants: {
+    size: {
+      xs: 'size-12',
+      sm: 'size-16',
+      md: 'size-20',
+      lg: 'size-24',
+      xl: 'size-32',
+      '2xl': 'size-40'
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+})
+
+export const Avatar: FC<AvatarProps> = ({
+  className = '',
+  size = 'md',
+  imgSrc = 'https://kr.cafe24obs.com/blog.brighteast/profile-image/default-profile.png',
+  ...props
+}) => {
+  return (
+    <div className={cn(AvatarVariants({ size }), className)} {...props}>
+      <img src={imgSrc} alt='Profile' className='block size-full' />
+    </div>
+  )
+}
---
+++ src/types/commonProps.d.ts
@@ -0,0 +1,1 @@
+export type sizeLiteral = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
