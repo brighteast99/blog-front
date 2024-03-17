8bda4c0d0dff6a327d5697114d75926927a11a00
brighteast99 <brighteast99@gmail.com>
2024-03-18 03:48:13 +0900

    feat: 컴포넌트 정의 (Spinner)

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,41 @@
+import { FC, HTMLAttributes } from 'react'
+import { sizeLiteral } from '../types/commonProps'
+import { cva } from 'class-variance-authority'
+import { cn } from '../utils/handleClassName'
+import Icon from '@mdi/react'
+import { mdiLoading } from '@mdi/js'
+
+export interface SpinnerProps extends HTMLAttributes<HTMLDivElement> {
+  /**
+   * Size of the spinner
+   */
+  size?: sizeLiteral
+}
+
+export const SpinnerVariants = cva(`aspect-square m-auto`, {
+  variants: {
+    size: {
+      xs: 'size-12',
+      sm: 'size-16',
+      md: 'size-20',
+      lg: 'size-24',
+      xl: 'size-32',
+      '2xl': 'size-40'
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+})
+
+export const Spinner: FC<SpinnerProps> = ({
+  className = '',
+  size = 'md',
+  ...props
+}) => {
+  return (
+    <div className={cn(SpinnerVariants({ size }), className)} {...props}>
+      <Icon path={mdiLoading} spin />
+    </div>
+  )
+}
