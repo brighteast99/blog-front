aacb81f5d4dec6b4a23604abb7655ab37e46c67e
brighteast99 <brighteast99@gmail.com>
2024-03-29 23:55:09 +0900

    fix: SuspendedText 수정
    로딩 후 텍스트에 lines 적용, 스켈레톤에 text-align 적용, 로딩 커서 적용, 상호작용 방지

--- src/components/SuspendedText.tsx
+++ src/components/SuspendedText.tsx
@@ -1,6 +1,8 @@
 import { FC, HTMLAttributes } from 'react'
 import { cn } from '../utils/handleClassName'
 import { PositiveInteger } from 'types/commonTypes'
+import { alignLiteral } from 'types/commonProps'
+import { cva } from 'class-variance-authority'
 
 export interface SuspendedTextProps extends HTMLAttributes<HTMLDivElement> {
   /**
@@ -10,7 +12,26 @@ export interface SuspendedTextProps extends HTMLAttributes<HTMLDivElement> {
   length?: number
   loading?: boolean
   lines?: PositiveInteger<number>
+  align?: alignLiteral
 }
+
+export const SuspendedTextVariants = cva(
+  `inline-flex max-w-full flex-col gap-2`,
+  {
+    variants: {
+      align: {
+        left: 'text-left items-left',
+        center: 'text-center items-center',
+        right: 'text-left items-right',
+        start: 'text-start items-start',
+        end: 'text-end items-end'
+      }
+    },
+    defaultVariants: {
+      align: 'start'
+    }
+  }
+)
 
 export const SuspendedText: FC<SuspendedTextProps> = ({
   className = '',
@@ -18,15 +39,16 @@ export const SuspendedText: FC<SuspendedTextProps> = ({
   loading,
   length = 1,
   lines = 1,
+  align = 'start',
   ...props
 }) => {
   if (loading)
     return (
-      <div className={cn(className, 'inline-flex max-w-full flex-col gap-2')}>
+      <div className={cn(className, SuspendedTextVariants({ align }))}>
         {Array.from({ length: lines }, (_val, i) => (
           <span
             key={i}
-            className='w-fit min-w-0 max-w-full animate-pulse rounded-full bg-neutral-800 text-transparent opacity-25'
+            className='pointer-none w-fit min-w-0 max-w-full animate-pulse cursor-wait rounded-full bg-neutral-800 text-transparent opacity-25'
             {...props}
           >
             {'◼︎'.repeat(lines === 1 || i < lines - 1 ? length : length / 5)}
@@ -35,7 +57,17 @@ export const SuspendedText: FC<SuspendedTextProps> = ({
       </div>
     )
   return (
-    <span className={className} {...props}>
+    <span
+      className={cn(className, SuspendedTextVariants({ align }))}
+      style={{
+        ...props?.style,
+        overflow: 'hidden',
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: lines
+      }}
+      {...props}
+    >
       {text}
     </span>
   )
--- src/types/commonProps.d.ts
+++ src/types/commonProps.d.ts
@@ -1,1 +1,2 @@
 export type sizeLiteral = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+export type alignLiteral = 'left' | 'center' | 'right' | 'start' | 'end'
