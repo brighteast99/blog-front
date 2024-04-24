8559cce265d109c49d5a19cd9c86e81dc17786a5
brighteast99 <brighteast99@gmail.com>
2024-04-24 13:49:15 +0900

    feat: method 추가, argument type 수정
    isPast, isFuture method 추가
    argument Date -> Date|number로 수정

--- src/utils/dayJS.ts
+++ src/utils/dayJS.ts
@@ -7,10 +7,21 @@ dayjs.extend(utc)
 dayjs.locale('ko')
 dayjs.extend(relativeTime)
 
-export function getRelativeTimeFromNow(date: Date): string {
+export function getRelativeTimeFromNow(date: Date | number): string {
   return dayjs().to(dayjs(date).format('YYYY-MM-DD HH:mm:ss'))
 }
 
-export function isSameTime(date1: Date, date2: Date): Boolean {
+export function isSameTime(
+  date1: Date | number,
+  date2: Date | number
+): Boolean {
   return dayjs(date1).isSame(dayjs(date2), 'milliseconds')
+}
+
+export function isPast(date: Date | number): Boolean {
+  return dayjs(date).isBefore(dayjs())
+}
+
+export function isFuture(date: Date | number): Boolean {
+  return dayjs(date).isAfter(dayjs())
 }
