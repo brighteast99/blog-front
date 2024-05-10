b455686652f592b226f41fe15c974b081a9d4f0b
brighteast99 <brighteast99@gmail.com>
2024-05-11 02:42:01 +0900

    feat: 게시글 삭제 기능

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
-import { TypedDocumentNode, gql, useQuery } from '@apollo/client'
+import { TypedDocumentNode, gql, useMutation, useQuery } from '@apollo/client'
 import { Post } from 'types/data'
 import { SuspendedText } from 'components/SuspendedText'
 import { Error } from 'components/Error'
@@ -12,6 +12,7 @@ import { useAppSelector } from 'app/hooks'
 import { selectIsAuthenticated } from 'features/auth/authSlice'
 import { PopoverMenu } from 'components/PopoverMenu'
 import { PopoverMenuItem } from 'components/PopoverMenu/PopoverMenuItem'
+import { GET_CATEGORIES } from 'features/sidebar/Sidebar'
 
 export const GET_POST: TypedDocumentNode<{ post: Post }, { id: number }> = gql`
   query PostData($id: Int!) {
@@ -32,6 +33,19 @@ export const GET_POST: TypedDocumentNode<{ post: Post }, { id: number }> = gql`
   }
 `
 
+const DELETE_POST: TypedDocumentNode<
+  {
+    deletePost: { success: boolean }
+  },
+  { id: number }
+> = gql`
+  mutation DeletePost($id: Int!) {
+    deletePost(id: $id) {
+      success
+    }
+  }
+`
+
 export const PostPage: FC = () => {
   const isLoggedIn = useAppSelector(selectIsAuthenticated)
   const location = useLocation()
@@ -42,9 +56,35 @@ export const PostPage: FC = () => {
     notifyOnNetworkStatusChange: true,
     skip: isNaN(Number(postId))
   })
+  const [_deletePost, { loading: deleting, reset: resetDeleteMutation }] =
+    useMutation(DELETE_POST)
 
   const isUpdated =
     !data?.post || !isSameTime(data.post.createdAt, data.post.updatedAt)
+
+  const deletePost = useCallback(() => {
+    if (!window.confirm('게시글을 삭제합니다.')) return
+
+    _deletePost({
+      variables: { id: Number(postId) },
+      refetchQueries: [
+        {
+          query: GET_CATEGORIES
+        }
+      ],
+      onCompleted: () => navigate(`/category/${data?.post.category.id}`),
+      onError: () => {
+        alert('게시글 삭제 중 문제가 발생했습니다.')
+        resetDeleteMutation()
+      }
+    })
+  }, [
+    _deletePost,
+    data?.post.category.id,
+    navigate,
+    postId,
+    resetDeleteMutation
+  ])
 
   if (error?.networkError)
     return (
@@ -118,7 +158,7 @@ export const PostPage: FC = () => {
               icon={mdiDelete}
               title='삭제'
               className='bg-error text-error'
-              onClick={() => {}}
+              onClick={deletePost}
             />
           </PopoverMenu>
         )}
