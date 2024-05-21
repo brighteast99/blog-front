6a0970072369e9a913b961a1abbe00ab3e6b59ff
brighteast99 <brighteast99@gmail.com>
2024-05-22 02:06:50 +0900

    feat: 게시글 임시 저장, 템플릿 기능

---
+++ src/pages/post/Edit/DraftManager.tsx
@@ -0,0 +1,202 @@
+import {
+  TypedDocumentNode,
+  gql,
+  useLazyQuery,
+  useMutation,
+  useQuery
+} from '@apollo/client'
+import { Placement } from '@floating-ui/react'
+import clsx from 'clsx'
+import { ThemedButton } from 'components/Buttons/ThemedButton'
+import { PopoverMenu } from 'components/PopoverMenu'
+import { Spinner } from 'components/Spinner'
+import { Tiptap } from 'components/Tiptap'
+import { FC, ReactNode, useCallback, useState } from 'react'
+import { Post } from 'types/data'
+import { getRelativeTimeFromNow } from 'utils/dayJS'
+
+export interface Draft extends Omit<Post, 'is_deleted|updated_at'> {
+  summary: string
+}
+
+export const GET_DRAFTS: TypedDocumentNode<{ draftList: Draft[] }> = gql`
+  query GetDrafts {
+    draftList {
+      id
+      summary
+      createdAt
+    }
+  }
+`
+
+export const GET_DRAFT: TypedDocumentNode<{ draft: Draft }, { id: number }> =
+  gql`
+    query GetDraft($id: Int!) {
+      draft(id: $id) {
+        id
+        summary
+        title
+        category {
+          id
+          name
+        }
+        isHidden
+        content
+        thumbnail
+        images
+      }
+    }
+  `
+
+export const DELETE_DRAFT: TypedDocumentNode<
+  { deleteDraft: { success: boolean } },
+  { id: number }
+> = gql`
+  mutation DeleteDraft($id: Int!) {
+    deleteDraft(id: $id) {
+      success
+    }
+  }
+`
+
+export interface DraftManagerProps {
+  className?: string
+  placement?: Placement
+  tooltipPlacement?: Placement
+  description?: string
+  children?: ReactNode
+  onSelect?: (data: Draft) => any
+}
+
+export const DraftManager: FC<DraftManagerProps> = ({
+  className,
+  placement = 'bottom-start',
+  tooltipPlacement = 'bottom',
+  description,
+  onSelect
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const { data: drafts, loading: loadingList } = useQuery(GET_DRAFTS)
+  const [selectedDraft, setSelectedDraft] = useState<number>()
+  const [loadTemplateInfo, { loading: loadingDraft, data: draft }] =
+    useLazyQuery(GET_DRAFT, {
+      fetchPolicy: 'cache-and-network'
+    })
+  const [_deleteDraft, { loading: deleting, reset: resetDeleteMutation }] =
+    useMutation(DELETE_DRAFT)
+
+  const deleteDraft = useCallback(() => {
+    if (!draft) return
+    if (
+      !window.confirm(`임시 저장본 '${draft.draft.summary}'을(를) 삭제합니다.`)
+    )
+      return
+
+    _deleteDraft({
+      variables: { id: Number(draft.draft.id) },
+      refetchQueries: [
+        {
+          query: GET_DRAFTS
+        }
+      ],
+      onCompleted: () => setSelectedDraft(undefined),
+      onError: () => {
+        alert('임시 저장본 삭제 중 문제가 발생했습니다.')
+        resetDeleteMutation()
+      }
+    })
+  }, [_deleteDraft, draft, resetDeleteMutation])
+
+  return (
+    <PopoverMenu
+      className={className}
+      open={isOpen}
+      placement={placement}
+      offset={12}
+      tooltipPlacement={tooltipPlacement}
+      description={description}
+      menuBtn={
+        <ThemedButton
+          className='h-8 px-2'
+          variant='hover-text'
+          disabled={loadingList || !drafts?.draftList.length}
+          color='primary'
+        >
+          {loadingList ? (
+            <Spinner size='xs' />
+          ) : (
+            `임시 저장본 ${drafts?.draftList.length ? `(${drafts.draftList.length})` : '없음'}`
+          )}
+        </ThemedButton>
+      }
+      onOpen={() => setIsOpen(true)}
+      onClose={() => setIsOpen(false)}
+    >
+      <div className='w-120 max-w-[90dvw] bg-neutral-50'>
+        <div className='flex h-40 flex-col border-b border-neutral-100'>
+          <div className='border-b border-neutral-100 bg-neutral-100 py-1 text-center'>
+            임시 저장본 목록
+          </div>
+          <ul className='min-h-0 grow overflow-y-auto'>
+            {drafts?.draftList.map((draft) => (
+              <li
+                className={clsx(
+                  'px-1 py-0.5',
+                  selectedDraft === draft.id
+                    ? 'bg-primary bg-opacity-25 hover:brightness-125'
+                    : 'hover:bg-background'
+                )}
+                key={draft.id}
+                onClick={() => {
+                  setSelectedDraft(draft.id)
+                  loadTemplateInfo({ variables: { id: draft.id } })
+                }}
+              >
+                {`${draft.summary} (${getRelativeTimeFromNow(draft.createdAt)})`}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {selectedDraft && (
+          <div className='relative flex h-100 flex-col'>
+            {loadingDraft || !draft ? (
+              <Spinner className='absolute inset-0 m-auto' />
+            ) : (
+              <>
+                <Tiptap
+                  className='-mx-2 my-2 flex-grow overflow-y-auto'
+                  editable={false}
+                  content={draft.draft.content}
+                />
+                <ThemedButton
+                  className='h-8 shrink-0 rounded-none'
+                  color='primary'
+                  variant='flat'
+                  disabled={deleting}
+                  onClick={() => {
+                    setSelectedDraft(undefined)
+                    onSelect?.(draft.draft)
+                    setIsOpen(false)
+                  }}
+                >
+                  불러오기
+                </ThemedButton>
+
+                <ThemedButton
+                  className='h-8 shrink-0 rounded-t-none'
+                  color='error'
+                  variant='text'
+                  disabled={deleting}
+                  onClick={deleteDraft}
+                >
+                  {deleting ? <Spinner size='xs' /> : '삭제'}
+                </ThemedButton>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </PopoverMenu>
+  )
+}
