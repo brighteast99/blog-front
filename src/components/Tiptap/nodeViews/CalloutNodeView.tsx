193714370e3fd732bb50ac6d4ada59260097499e
brighteast99 <brighteast99@gmail.com>
2024-09-13 13:10:09 +0900

    Feat(Tiptap): 노드 스타일 업데이트
    링크 스타일 개선
    콜아웃 스타일 개선
    볼드, 이탤릭, 취소선 input rule, paste rule 비활성화

--- src/components/Tiptap/nodeViews/CalloutNodeView.tsx
+++ src/components/Tiptap/nodeViews/CalloutNodeView.tsx
@@ -18,36 +18,42 @@ import type { CalloutType } from 'components/Tiptap/extensions/Callout'
 
 interface CalloutComponentProps extends NodeViewProps {}
 
-export const CalloutVariants = cva('ml-2 border-l-4 bg-opacity-10 p-4', {
-  variants: {
-    type: {
-      note: 'border-primary bg-primary',
-      info: 'border-info bg-info',
-      success: 'border-success bg-success',
-      question: 'border-teal-800 bg-teal-800',
-      caution: 'border-warning bg-warning',
-      warning: 'border-error bg-error'
+export const CalloutVariants = cva(
+  'ml-2 border-l-[5px] bg-opacity-15 px-3 py-2.5 rounded my-4 border-opacity-75',
+  {
+    variants: {
+      type: {
+        note: 'border-primary bg-primary',
+        info: 'border-info bg-info',
+        success: 'border-success bg-success',
+        question: 'border-teal-800 bg-teal-800',
+        caution: 'border-warning bg-warning',
+        warning: 'border-error bg-error'
+      }
+    },
+    defaultVariants: {
+      type: 'note'
     }
-  },
-  defaultVariants: {
-    type: 'info'
   }
-})
-export const CalloutIconVariants = cva('brightness-110', {
-  variants: {
-    type: {
-      note: 'text-primary',
-      info: 'text-info',
-      success: 'text-success',
-      question: 'text-teal-800',
-      caution: 'text-warning',
-      warning: 'text-error'
+)
+export const CalloutTitleVariants = cva(
+  'mb-5 flex items-center gap-2 text-lg font-semibold brightness-110',
+  {
+    variants: {
+      type: {
+        note: 'text-primary',
+        info: 'text-info',
+        success: 'text-success',
+        question: 'text-teal-800',
+        caution: 'text-warning',
+        warning: 'text-error'
+      }
+    },
+    defaultVariants: {
+      type: 'note'
     }
-  },
-  defaultVariants: {
-    type: 'note'
   }
-})
+)
 
 export const CalloutNodeView: FC<CalloutComponentProps> = ({ node }) => {
   const type: CalloutType = node.attrs.type || 'info'
@@ -70,13 +76,9 @@ export const CalloutNodeView: FC<CalloutComponentProps> = ({ node }) => {
 
   return (
     <NodeViewWrapper as='blockquote' className={CalloutVariants({ type })}>
-      <div className='mb-5 flex items-center gap-2' contentEditable={false}>
-        <Icon
-          className={CalloutIconVariants({ type })}
-          path={iconPath}
-          size='1.75rem'
-        />
-        <span className='text-xl font-bold capitalize'>{type}</span>
+      <div className={CalloutTitleVariants({ type })} contentEditable={false}>
+        <Icon path={iconPath} size='1.25rem' />
+        <span className='capitalize'>{type}</span>
       </div>
       <NodeViewContent />
     </NodeViewWrapper>
